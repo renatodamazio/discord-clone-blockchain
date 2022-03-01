83890fd596665ce3fbdf34b28534ebcd99d00e26
Renato <renatopdamazio@gmail.com>
2022-03-01 15:40:46 -0300

    Server heroku configuration done

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,7 +1,8 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import { userSchema } from "./users";
+import { conversationSchema } from "./conversationSchema";
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([]),
-})
+  name: "default",
+  types: schemaTypes.concat([userSchema, conversationSchema]),
+});
