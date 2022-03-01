51e7ff2e0bc92cb1d602935bf54d43e0d008c287
Renato <renatopdamazio@gmail.com>
2022-03-01 17:26:25 -0300

    Room avatar component

--- client/components/Sider.jsx
+++ client/components/Sider.jsx
@@ -1,9 +1,53 @@
 import styles from "../styles/Sidebar.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import avatar1 from "../public/assets/avatar-1.png";
+import avatar2 from "../public/assets/avatar-2.png";
+import avatar3 from "../public/assets/avatar-3.png";
+import avatar4 from "../public/assets/avatar-4.png";
+
+import RoomAvatar from "../components/RoomAvatar";
+
+const dummyChannels = [
+  {
+    id: 1,
+    name: "general",
+    avatar: avatar1,
+  },
+  {
+    id: 2,
+    name: "random",
+    avatar: avatar2,
+  },
+  {
+    id: 3,
+    name: "chill",
+    avatar: avatar3,
+  },
+  {
+    id: 4,
+    name: "buildspace",
+    avatar: avatar4,
+  },
+];
 
 const Sidebar = () => {
-    return (
-        <div className={styles.wrapper}>Sider bar</div>
-    )
-}
+  const router = useRouter();
+  const [channels, setChannels] = useState(dummyChannels);
+
+  return (
+    <div className={styles.wrapper}>
+      {channels.map((channel) => (
+        <RoomAvatar
+          key={channel.id}
+          id={channel.id}
+          avatar={channel.avatar}
+          name={channel.roomName}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Sidebar;
