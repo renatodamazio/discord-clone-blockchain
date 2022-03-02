57cc26786ccd74b704383436ce07298a92d57cb3
Renato <renatopdamazio@gmail.com>
2022-03-02 16:35:13 -0300

    Get channels from sanity

--- client/pages/api/getchannels.js
+++ client/pages/api/getchannels.js
@@ -1,22 +1,14 @@
 import { client } from "../../lib/client";
 
+const query = `*[_type == "conversations" && isDm==false] {
+  roomId,
+  roomName,
+  "avatar": image.asset->url
+}`;
+
 export default async (req, res) => {
-  const { userAddress } = req.body;
-
-  const conversationDoc = {
-    _type: "conversations",
-    _id: `${userAddress}-dm`,
-    isDm: true,
-    roomId: userAddress,
-    userReference: {
-      _key: userAddress,
-      _ref: `${userAddress}-user`,
-      _type: "refrence",
-    },
-  };
-
   try {
-    await client.createIfNotExists(conversationDoc);
+    const sanityResponse = await client.fetch(query);
     res.status(200).send("Successfull");
   } catch (error) {
     res.status(500).send(error);
