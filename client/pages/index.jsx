a801013734b3ba9a46e5d34c4da6bc35ca3e96ac
Renato <renatopdamazio@gmail.com>
2022-03-03 13:09:15 -0300

    meta description

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -2,9 +2,17 @@ import styles from "../styles/Home.module.css";
 import Sidebar from "../components/Sider";
 import ConversationList from "../components/ConversationList";
 import ChatView from "../components/ChatView";
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div className={styles.wrapper}>
+      <Head>
+        <title>Discord Clone</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
+        <meta name="description" content="Discord clone React, gunjs, Vercel, Next Js, CSS, web3.0, metamask" />
+        <meta name="author" content="Renato Damázio" />
+      </Head> 
       <Sidebar />
       <div className={styles.main}>
         <ConversationList />
