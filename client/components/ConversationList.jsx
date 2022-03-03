3d8e7cdcc3ca05bd62741ade5d6f89f7b785ec8c
Renato <renatopdamazio@gmail.com>
2022-03-02 23:41:07 -0300

    Loading content sidebar items

--- client/components/ConversationList.jsx
+++ client/components/ConversationList.jsx
@@ -5,36 +5,18 @@ import styles from "../styles/ConversationList.module.css";
 import friends from "../public/assets/icons/friends.svg";
 import nitro from "../public/assets/icons/nitro.svg";
 import DmCard from "./DmCard";
-import avatar1 from "../public/assets/avatar-1.png";
-import avatar2 from "../public/assets/avatar-2.png";
-import avatar3 from "../public/assets/avatar-3.png";
-import avatar4 from "../public/assets/avatar-4.png";
-
-const dummyDms = [
-  {
-    id: 1,
-    name: "Renato",
-    avatar: avatar1,
-  },
-  {
-    id: 2,
-    name: "Amanda",
-    avatar: avatar2,
-  },
-  {
-    id: 3,
-    name: "João",
-    avatar: avatar3,
-  },
-  {
-    id: 4,
-    name: "Fernanda",
-    avatar: avatar4,
-  },
-];
 
 export default function ConversationList() {
-  const [dms, setDms] = useState(dummyDms);
+  const [dms, setDms] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API}/getdm`)
+      setDms(await response.json());
+    } catch(err) {
+      console.error(err);
+    }
+  }, [])
 
   return (
     <div className={styles.conversations}>
@@ -67,19 +49,21 @@ export default function ConversationList() {
           </div>
           <p>Nitro</p>
         </div>
-        <div className={styles.dmTitle}>DIRECT MESSAGES</div>
-        {dms.map((dm) => (
-          <DmCard
-            key={dm.id}
-            name={dm.name}
-            id={dm.id}
-            status="online"
-            avatar={
-              dm.avatar ||
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFI"
-            }
-          />
-        ))}
+        <div className={styles.conversationsContainer}>
+          <div className={styles.dmTitle}>DIRECT MESSAGES</div>
+          {dms.map((dm) => (
+            <DmCard
+              key={dm.id}
+              name={dm.name}
+              id={dm.id}
+              status="online"
+              avatar={
+                dm.avatar ||
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFI"
+              }
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
